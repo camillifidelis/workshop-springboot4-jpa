45f25d0cf68ca00b03d52ed6fa125f77700fd2a9
camillifidelis <cfidelisg@gmail.com>
2026-04-08 18:30:49 -0300

    Exception handling - delete

--- src/main/java/com/camillifidelis/webservicesjpa/services/UserService.java
+++ src/main/java/com/camillifidelis/webservicesjpa/services/UserService.java
@@ -2,10 +2,15 @@ package com.camillifidelis.webservicesjpa.services;
 
 import java.util.List;
 import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
+
 import com.camillifidelis.webservicesjpa.entities.User;
 import com.camillifidelis.webservicesjpa.repositories.UserRepository;
+import com.camillifidelis.webservicesjpa.services.exceptions.DatabaseException;
 import com.camillifidelis.webservicesjpa.services.exceptions.ResourceNotFoundException;
 
 @Service
@@ -28,7 +33,13 @@ public class UserService {
 	}
 	
 	public void delete(Long id) {
+		try {
 		repository.deleteById(id);
+		} catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+		}
 	}
 	
 	public User update(Long id, User obj) {
